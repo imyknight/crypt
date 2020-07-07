83515b73ba8d336fe7626924e88c1aae3a3572b7
hungle965 <hungcan1998965@gmail.com>
2020-07-07 22:49:11 +0700

    delete server

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import '../styles/header.scss'
+import { withRouter } from 'react-router'
+import { getToken, deleteToken } from '../token'
 
-function Header({ children }) {
+const Header = (props, { children }) => {
+  const isLoggedIn = !!getToken();
+  console.log(isLoggedIn)
   return (
     <div>
       <div className="nav">
         <div className="nav-item">
           <Link to='/'>
-            <div className="logo">logo</div>
+            <div className="logo">
+              <img className="img-logo" alt="logo" src="/logo@3x.png" />
+            </div>
           </Link>
           <div className="list-item">
             <div className="item">item</div>
@@ -16,18 +22,36 @@ function Header({ children }) {
             <div className="item">item</div>
           </div>
         </div>
-        <div className="sign">
-          <Link to="/sign-in">
-            <div className="signin">
-              sign in
-          </div>
-          </Link>
-          <Link to='/sign-up'>
-            <div className="signup">
-              sign up
-          </div>
-          </Link>
-        </div>
+        {
+          !isLoggedIn ?
+            <div className="sign">
+              <Link to="/sign-in">
+                <div className="signin">
+                  sign in
+                  </div>
+              </Link>
+              <Link to='/sign-in'>
+                <div className="signup">
+                  sign up
+                </div>
+              </Link>
+            </div> :
+            <div className="sign">
+              <div className="signin">
+                welcome
+              </div>
+              <div 
+                className="signup"
+                onClick={ () => {
+                  deleteToken();
+                  props.history.push('/')
+                } }
+              >
+                logout
+              </div>
+            </div>
+        }
+
       </div>
       <div>
         {children}
@@ -36,4 +60,4 @@ function Header({ children }) {
   )
 }
 
-export default Header
+export default withRouter(Header)
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,46 @@
 import React from 'react'
 import '../styles/login.scss'
 import { Link } from 'react-router-dom'
+import { setToken } from '../token'
+import gql from 'graphql-tag'
+import { useMutation } from 'urql'
 
-function Login() {
-  const [user, setUser] = React.useState('');
-  const [passWord, setPassWord] = React.useState('');
-
-  const login = () => {
-    console.log(user, passWord)
+const SIGNUP_MUTATION = gql`
+  mutation SignupMutation($email: String!, $password: String!, $name: String!) {
+    signup(email: $email, password: $password, name: $name) {
+      token
+    }
   }
+`
+const LOGIN_MUTATION = gql`
+  mutation LoginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`
+
+function Login(props) {
+  const [isLogin, setIsLogin] = React.useState('');
+
+  const [name, setName] = React.useState('');
+  const [password, setPassWord] = React.useState('');
+  const [email, setEmail] = React.useState('');
+
+  const [state, executeMutation] = useMutation(
+    isLogin ? LOGIN_MUTATION : SIGNUP_MUTATION
+  )
+
+  const mutate = React.useCallback(() => {
+    executeMutation({ email, password, user: name })
+      .then(({ data }) => {
+        if (data) {
+          const token = data && data[isLogin ? 'login' : 'signup'].token
+          setToken(token)
+          props.history.push('/')
+        }
+      })
+  }, [executeMutation, props.history, isLogin, email, password, name])
 
   return (
     <div className="login">
@@ -21,18 +53,36 @@ function Login() {
           </div>
         </Link>
         <div className="pls-login">
-          <p>Please Login to continue</p>
+          {
+            state.fetching ? <img className="img-loading" src="/loading.png" alt="loading" /> :
+              isLogin ?
+                <p>Please Login to continue</p> :
+                <p>Register new account</p>
+          }
+
         </div>
         <div className="input-section">
-          <input placeholder="username" onChange={e => setUser(e.target.value)} />
+          {
+            !isLogin && (
+              <input placeholder="username" onChange={e => setName(e.target.value)} />
+            )
+          }
+          <input placeholder="email" onChange={e => setEmail(e.target.value)} />
           <input placeholder="password" type="password" onChange={e => setPassWord(e.target.value)} />
         </div>
         <div className="l-btn">
-          <div className="btn-su">
-            Sign Up
+          <div
+            className="btn-su"
+            onClick={mutate}
+          >
+            {isLogin ? "login" : "create account"}
           </div>
-          <div className="btn-li" onClick={login}>
-            Login
+          <div
+            className="btn-li"
+            disable={state.fetching}
+            onClick={() => setIsLogin(!isLogin)}
+          >
+            {isLogin ? "need to create account" : "have an account?"}
           </div>
         </div>
       </div>
